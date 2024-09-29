d59c021de1463cd51e84f845250851cd7a440ce7
Matthew Hyatt <mhyatt000@gmail.com>
2024-09-29 17:47:39 -0500

    nvim deploy

--- scripts/test_safety_box.py
+++ scripts/test_safety_box.py
@@ -1,4 +1,7 @@
 import xarm_gym as xg
 from xarm_gym import utils as xgu
 
-b= xgu.CartesianBoundary(
+b = xgu.CartesianBoundary(
+    xgu.PartialRobotState([1, 2, 3]), xgu.PartialRobotState([4, 5, 6])
+    )
+)
