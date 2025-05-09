2bd966321740352a918fe9cdbeb2e40af6a1f752
matt <mhyatt@luc.edu>
2025-05-09 21:33:23 +0000

    clean depepndency optional/extras

--- setup.py
+++ setup.py
@@ -8,14 +8,4 @@ setup(
     author='Matt Hyatt',
     author_email='mhyatt000@gmail.com',
     url='https://github.com/mhyatt000/xarm-gym',
-    install_requires=[ ],
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-    ],
 )
