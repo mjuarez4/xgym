3f0005fd1a3798ff3b75dc8fd91a589dc8af0cc6
Matt Hyatt <mhyatt000@gmail.com>
2025-05-12 22:37:56 -0500

    push new reader- fix linter

--- scripts/reader_rlds.py
+++ scripts/reader_rlds.py
@@ -4,18 +4,21 @@ from pprint import pprint
 from typing import Union
 
 import cv2
-import draccus
+import jax
 import jax.numpy as jnp
+import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow_datasets as tfds
-import xgym
+import tyro
+from jax import numpy as jnp
 from tqdm import tqdm
+
+import xgym
 from xgym.rlds.util import add_col, remove_col
 from xgym.rlds.util.render import render_openpose
 from xgym.rlds.util.trajectory import binarize_gripper_actions, scan_noop
 from xgym.viz.mano import overlay_palm, overlay_pose
 
-import tyro
 
 class Task(enum.Enum):
     LIFT = "lift"
@@ -32,14 +35,18 @@ class Embodiment(enum.Enum):
 @dataclass
 class BaseReaderConfig:
 
+    version: str  # (e.g. "4.0.3")
     embodiment: Embodiment = Embodiment.SINGLE
     task: Task = Task.LIFT
-    version: str = tyro.MISSING
+
     verbose: bool = False
     visualize: bool = True
 
+    show_image: bool = True  # replacement for visualize
+    show_summary: bool = False
+
     use_palm: bool = False
-    wait: int = 20 # the cv2 wait time in ms 
+    wait: int = 20  # the cv2 wait time in ms
     horizon: int = 10
     resolution: int = 3
 
@@ -103,9 +110,7 @@ class RLDSReader(Reader):
 
                     if cfg.use_palm:
                         # z = np.array(steps[i + j]["observation"]["keypoints_3d"])[0][2]
-                        img = overlay_palm(
-                            img, int(palm[0]), int(palm[1]), opacity=j
-                        )
+                        img = overlay_palm(img, int(palm[0]), int(palm[1]), opacity=j)
                     else:
                         img = overlay_pose(img, points2d, opacity=j)
                     # ifprint(palm)
@@ -119,9 +124,56 @@ class RLDSReader(Reader):
             imshow("image", cv2.resize(recolor(img), (640, 640)))
 
 
-import tyro
-import jax
-from jax import numpy as jnp
+def summary():
+
+    fig, axs = plt.subplots(14, 1, figsize=(20, 40))
+
+    ds = ds.take(100)
+
+    reader = RLDSReader(cfg)
+    for ep in tqdm(ds, total=len(ds)):
+        steps = [x for x in ep["steps"]]
+        # for i, s in tqdm(enumerate(steps), total=len(steps), leave=False):
+
+        joints = [s["observation"]["proprio"]["joints"] for s in steps]
+        grip = [s["observation"]["proprio"]["gripper"] for s in steps]
+
+        selected = np.concatenate([joints, grip], axis=1)
+        ### filter joints noop
+        noops = np.array(scan_noop(jnp.array(selected), threshold=1e-2))
+        mask = ~noops
+        # ep = jax.tree.map(select := lambda x: x[mask], ep)
+
+        print(mask)
+        # steps = [s for s, m in zip(steps, mask) if m]
+        # quit()
+
+        # combine list[dict[str, np.array]] into dict[str, stacked np.array]
+        outs = {}
+        outs2 = {}
+
+        # for k in steps[0]["action"].keys():
+        # outs[k] = np.stack([s["action"][k] for s in steps])
+        # outs2[k] = np.stack([s["action"][k] for s in steps])
+        # for k in steps[0]['observation']['proprio'].keys():
+        # outs[k] = np.stack([s['observation']['proprio'][k] for s in steps])
+        # outs2[k] = np.stack([s['observation']['proprio'][k] for s in steps])
+
+        outs = np.concatenate(list(outs.values()), axis=1)
+        outs2 = np.concatenate(list(outs2.values()), axis=1)
+        outs2 = outs2[mask]
+
+        for i in range(outs.shape[1]):
+            # ax[i].plot(outs[:,i],color=(0,0,1,0.05))
+            axs[i].plot(outs[:, i], color=(0, 0, 1, 1))
+            axs[i].plot(outs2[:, i], color=(0, 0, 1, 1))
+            axs[i].plot(np.zeros_like(outs[:, i]), color=(0, 0, 0, 0.5), linestyle="--")
+
+        plt.pause(2)
+        # clear all the axes
+        for ax in axs:
+            ax.clear()
+
 
 def main(cfg: BaseReaderConfig):
     pprint(cfg)
@@ -133,18 +185,28 @@ def main(cfg: BaseReaderConfig):
 
     # ds = ds.repeat(100)
 
+    def ifprint(*args, **kwargs):
+        if cfg.verbose:
+            print(*args, **kwargs)
+
+    def ifshow(*args, **kwargs):
+        if cfg.visualize:
+            cv2.imshow(*args, **kwargs)
+
+    """
     import matplotlib.pyplot as plt
+
     fig, axs = plt.subplots(14, 1, figsize=(20, 40))
 
-    ds = ds.take(100)
+    ds = ds.take(10)
 
     reader = RLDSReader(cfg)
     for ep in tqdm(ds, total=len(ds)):
         steps = [x for x in ep["steps"]]
         # for i, s in tqdm(enumerate(steps), total=len(steps), leave=False):
 
-        joints = [s['observation']["proprio"]['joints'] for  s in steps]
-        grip [ s['observation']["proprio"]['gripper'] for  s in steps]
+        joints = [s["observation"]["proprio"]["joints"] for s in steps]
+        grip = [s["observation"]["proprio"]["gripper"] for s in steps]
 
         selected = np.concatenate([joints, grip], axis=1)
         ### filter joints noop
@@ -152,37 +214,44 @@ def main(cfg: BaseReaderConfig):
         mask = ~noops
         # ep = jax.tree.map(select := lambda x: x[mask], ep)
 
-        print(mask)
-        quit()
-
+        # print(mask)
+        # steps = [s for s, m in zip(steps, mask) if m]
+        # quit()
 
         # combine list[dict[str, np.array]] into dict[str, stacked np.array]
         outs = {}
-        for k in steps[0]['action'].keys():
-            outs[k] = np.stack([s['action'][k] for s in steps])
+        outs2 = {}
+
+        for k in steps[0]["action"].keys():
+            outs[k] = np.stack([s["action"][k] for s in steps])
+        # outs2[k] = np.stack([s["action"][k] for s in steps])
         # for k in steps[0]['observation']['proprio'].keys():
-            # outs[k] = np.stack([s['observation']['proprio'][k] for s in steps])
+        # outs[k] = np.stack([s['observation']['proprio'][k] for s in steps])
+        # outs2[k] = np.stack([s['observation']['proprio'][k] for s in steps])
 
         outs = np.concatenate(list(outs.values()), axis=1)
+        # outs2 = np.concatenate(list(outs2.values()), axis=1)
+        # outs2 = outs2[mask]
 
         for i in range(outs.shape[1]):
             # ax[i].plot(outs[:,i],color=(0,0,1,0.05))
-            axs[i].plot(outs[:,i],color=(0,0,1,1))
-            axs[i].plot(np.zeros_like(outs[:,i]),color=(0,0,0,0.5),linestyle='--')
+            # axs[i].plot(outs[:, i], color=(0, 0, 1, 0.05))
+            axs[i].plot(outs[:, i], color=(0, 0, 1, 1))
+            axs[i].plot(np.zeros_like(outs[:, i]), color=(0, 0, 0, 0.5), linestyle="--")
 
-        plt.pause(0.5)
+        # plt.pause(2)
         # clear all the axes
-        for ax in axs:
-            ax.clear()
+        # for ax in axs:
+        # ax.clear()
+
+    plt.show()
+    quit()
+    # plt.plot
+
+    # reader.read(s)
 
     quit()
-        # plt.plot
-
-            # reader.read(s)
-
-    quit()
-
-
+    """
 
     allnoops = 0
     for ep in tqdm(ds, total=len(ds)):
@@ -218,7 +287,7 @@ def main(cfg: BaseReaderConfig):
             if cfg.embodiment == "single":
                 ifprint(actions[i].round(2))
                 img = np.concatenate(list(s["observation"]["image"].values()), axis=1)
-                imshow("image", recolor(img))
+                ifshow("image", recolor(img))
 
             else:
                 try:
@@ -247,15 +316,17 @@ def main(cfg: BaseReaderConfig):
                         # ifprint(palm)
 
                         wrist = np.array(s["observation"]["img_wrist"])
-                        imshow("wrist", cv2.resize(recolor(wrist), (640, 640)))
+                        ifshow("wrist", cv2.resize(recolor(wrist), (640, 640)))
 
                     except (IndexError, KeyError) as e:
                         pass
                         # ifprint(e)
-                imshow("image", cv2.resize(recolor(img), (640, 640)))
+                ifshow("image", cv2.resize(recolor(img), (640, 640)))
 
             if cfg.visualize:
-                cv2.waitKey(cfg.wait)
+                key = cv2.waitKey(cfg.wait)
+                if key == ord("q"):
+                    break
 
         print()
     ifprint(allnoops)
