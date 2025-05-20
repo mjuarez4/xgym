9424a8d97ae604f9ebf7bba2c3c41e516115843c
Matt Hyatt <mhyatt000@gmail.com>
2025-05-20 11:36:58 -0500

    chmod

--- xgym/rlds/__init__.py
+++
@@ -1,5 +0,0 @@
-from .xgym_lift_single import XgymLiftSingle
-from .xgym_play_single import XgymPlaySingle
-from .xgym_duck_single import XgymDuckSingle
-from .xgym_stack_single import XgymStackSingle
-from .xgym_lift_mano import XgymLiftMano
